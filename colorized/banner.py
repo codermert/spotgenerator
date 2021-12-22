261d6f7540067347828e8c188ba1adea48aa0a8a
Mert Salik <53333294+codermert@users.noreply.github.com>
2021-12-22 22:22:56 +0300

    Add files via upload

---
+++ colorized/banner.py
@@ -0,0 +1,20 @@
+from .colors import *
+
+def banner():
+    banner = f'''
+
+
+%s%s _________     _________                  ______  ___           _____ 
+%s%s __  ____/___________  /____________      ___   |/  /_____________  /_
+%s%s _  /    _  __ \  __  /_  _ \_  ___/      __  /|_/ /_  _ \_  ___/  __/
+%s%s / /___  / /_/ / /_/ / /  __/  /          _  /  / / /  __/  /   / /_  
+%s%s \____/  \____/\__,_/  \___//_/           /_/  /_/  \___//_/    \__/  
+%s%s
+                                                             
+
+                                 %s%sVersion    : %s%s1.4
+                                 %s%sDeveloper  : %s%sCoder Mert 
+                                 %s%sGithub     : %s%shttps://github.com/codermert
+
+''' % (fc, sb,fc, sb,fc, sb, fm, sb, fy, sb, fy, sb, fy,sd, fg, sd, fy, sd, fg, sd, fy, sd, fg, sd)
+    return banner
